83a8c118ab3fc63225282ba29dd973bdd41035c9
Lester Chan <lesterchan@gmail.com>
2026-07-26 11:01:07 +0800

    Bump WP 6.0 PHP 7.4

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Contributors: GamerZ  
 Donate link: https://lesterchan.net/site/donation/  
 Tags: show, hide, toggle, visibility, press release  
-Requires at least: 4.6  
+Requires at least: 6.0  
 Tested up to: 7.0  
 Stable tag: 2.0.0  
 Requires PHP: 7.4  
--- wp-showhide.php
+++ wp-showhide.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://lesterchan.net/portfolio/programming/php/
  * Description: Allows you to embed content within your blog post via WordPress ShortCode API and toggling the visibility of the content via a link. By default the content is hidden and user will have to click on the "Show Content" link to toggle it. Similar to what Engadget is doing for their press releases. Example usage: <code>[showhide type="pressrelease"]Press Release goes in here.[/showhide]</code>
  * Version: 2.0.0
- * Requires at least: 4.6
+ * Requires at least: 6.0
  * Requires PHP: 7.4
  * Author: Lester 'GaMerZ' Chan
  * Author URI: https://lesterchan.net
